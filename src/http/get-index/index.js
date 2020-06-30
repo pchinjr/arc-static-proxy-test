36241b9f7bd176ce6204b8b490944a23ba57e315
Paul Chin Jr <pchinjr@gmail.com>
2020-06-29 21:53:01 -0400

    removed proxy plugin

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,10 +1,6 @@
 let arc = require('@architect/functions')
 
 exports.handler = arc.http.proxy({
-  plugins: {
-    html: ['@architect/proxy-plugin-html-urls'],
-    css: ['@architect/proxy-plugin-css-urls']
-  },
   alias: {
     '/about':'/about.html'
   }
